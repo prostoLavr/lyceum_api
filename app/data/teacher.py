8d9d17a0b86ee67bc4c0af1eee67575c14a8b968
Lawrence Naumov <prostolawr@gmail.com>
2022-10-16 12:57:24 +0800

    Move teacher from school to subject

--- app/data/teacher.py
+++ app/data/teacher.py
@@ -8,6 +8,4 @@ class Teacher(SqlAlchemyBase):
     teacher_id = sa.Column(sa.Integer, primary_key=True, 
                            autoincrement=True, index=True)
     name = sa.Column(sa.VARCHAR(128))
-    school_id = sa.Column(sa.Integer, sa.ForeignKey("schools.school_id"), 
-                          nullable=False)
     
