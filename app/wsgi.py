6b300704626b14794090e535e7ac86ea548b7dc0
Lawrence Naumov <prostolawr@gmail.com>
2022-10-23 01:03:33 +0800

    Clear code

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 
 from sqlalchemy_sessions import global_init
 import db_manager
@@ -9,8 +9,7 @@ from dataclasses import dataclass
 from typing import Optional
 import json
 
-
-# Lawrence's shit code to run with args 
+# Lawrence's shit code to run with args
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sqlite')
@@ -19,11 +18,13 @@ if __name__ == "__main__":
     parser.add_argument('-H', '--host')
     parser.add_argument('-d', '--database')
     parser.add_argument('-p', '--port')
-    
+
     args = parser.parse_args()
     sqlite_path = getattr(args, 'sqlite')
 else:
     sqlite_path = None
+
+
     # It's is real trash
     @dataclass
     class VoidArgs:
@@ -32,10 +33,10 @@ else:
         database: None
         port: None
         host: None
-    
+
+
     args = VoidArgs(None, None, None, None, None)
     # More Nones
-    
 
 if sqlite_path is None:
     user = args.user or os.getenv("POSTGRES_USER")
@@ -51,15 +52,15 @@ wsgi_app = Flask(__name__)
 
 db_manager.create_default()
 
-
 lessons_type = dict[str, dict[str, str or bool or list[list[int]]]]
+error_msg_type = dict[str, str]
+page_type = str | bytes
 
 
 def get_lessons(
         school_id: Optional[int] = None,
         school_class_id: Optional[int] = None
-        ) -> lessons_type:
-
+) -> lessons_type | error_msg_type:
     if school_id is not None:
         return db_manager.get_lessons_by_school_id(school_id)
     elif school_class_id is not None:
@@ -67,32 +68,33 @@ def get_lessons(
     else:
         raise ValueError("Expected school_id or school_class_id")
 
-    return {"msg": "Sorry, it yet does not work"}
 
-def to_json(data: dict) -> str:
-    print(f'{data=}')
+def to_json(data: dict) -> bytes:
     return json.dumps(data, ensure_ascii=False).encode("utf8")
 
+
 @wsgi_app.route("/school")
-def schools() -> str:
+def schools() -> page_type:
     return to_json(db_manager.get_schools())
 
+
 @wsgi_app.route("/school/<int:school_id>/school_class")
-def school_classes_list(school_id: int) -> str:
+def school_classes_list(school_id: int) -> page_type:
     schools = db_manager.get_school_classes_by_school_id(school_id)
     return to_json(schools)
 
+
 @wsgi_app.route("/school/<int:school_id>/lesson")
-def lessons_for_school(school_id: int) -> str:
+def lessons_for_school(school_id: int) -> page_type:
     lessons = get_lessons(school_id=school_id)
     return to_json(lessons)
 
+
 @wsgi_app.route("/school_class/<int:school_class_id>/lesson")
-def lessons_for_school_class(school_class_id: int) -> str:
+def lessons_for_school_class(school_class_id: int) -> page_type:
     lessons = get_lessons(school_class_id=school_class_id)
     return to_json(lessons)
 
 
 if __name__ == "__main__":
     wsgi_app.run("0.0.0.0", 8080)
-
